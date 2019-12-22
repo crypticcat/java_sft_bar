e547e848aaf9d808da18043f19e1eb7a2eecfcc6
crypticcat <87na.vi@gmail.com>
2019-12-22 12:10:22 +0300

    implemented distance calculation using method in Point class

--- sandbox/src/main/java/sft/bar/sandbox/Point.java
+++ sandbox/src/main/java/sft/bar/sandbox/Point.java
@@ -1,12 +1,15 @@
 package sft.bar.sandbox;
 
 public class Point {
-    public double x;
-    public double y;
+    public double x, y;
 
     public Point (double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double distance (Point p1) {
+        return Math.round(Math.hypot(p1.x - this.x, p1.y - this.y));
 
     }
 
--- sandbox/src/main/java/sft/bar/sandbox/TaskTwo.java
+++ sandbox/src/main/java/sft/bar/sandbox/TaskTwo.java
@@ -7,10 +7,6 @@ public class TaskTwo {
         Point p1 = new Point(7.0, 5.0);
         Point p2 = new Point(5.0, 7.0);
 
-        System.out.println("Расстояние между точками c координатами " + p1.x + ", " + p1.y + " и " + p2.x + ", " + p2.y + " равно: " + distance (p1, p2));
-    }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.round(Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2)));
+        System.out.println("Расстояние между точками c координатами " + p1.x + ", " + p1.y + " и " + p2.x + ", " + p2.y + " равно: " + p1.distance(p2));
     }
 }
