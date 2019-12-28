f0e9e9aa2a4bb0f62437f44d323601974c5ea273
Nadezhda_Barsukova <87na.vi@gmail.com>
2019-12-28 03:30:14 -0500

    created auxiliary class GroupData and put fillGroupForm parameters into the object

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/GroupCreationTests.java
@@ -36,7 +36,7 @@ public class GroupCreationTests {
     public void testGroupCreation() throws Exception {
         gotoGroupPage();
         initGroupCreation("new");
-        fillGroupForm("NadyaTest", "NadyaTest1", "NadyaTest2");
+        fillGroupForm(new GroupData("NadyaTest", "NadyaTest1", "NadyaTest2"));
         submitGroupCreation();
         returnToGroupPage();
         logout();
@@ -54,17 +54,17 @@ public class GroupCreationTests {
         wd.findElement(By.name("submit")).click();
     }
 
-    private void fillGroupForm(String name, String header, String footer) {
+    private void fillGroupForm(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Groups'])[1]/following::form[1]")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(name);
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(header);
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(footer);
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
     private void initGroupCreation(String s) {
