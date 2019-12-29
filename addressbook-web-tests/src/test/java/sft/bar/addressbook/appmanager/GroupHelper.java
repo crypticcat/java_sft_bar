0923209ab2e74b229c2a67552767669b7028858c
Nadezhda_Barsukova <87na.vi@gmail.com>
2019-12-29 09:08:08 -0500

    created HelperBase and modified NavigationHelper, SessionHelper, GroupHelper, ContactHelper, ApplicationManager

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/GroupHelper.java
@@ -4,43 +4,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import sft.bar.addressbook.model.GroupData;
 
-public class GroupHelper {
-    private WebDriver wd;
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void returnToGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
+        click(By.name("submit"));
     }
 
     public void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Groups'])[1]/following::form[1]")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
     }
 
     public void initGroupCreation(String s) {
-        wd.findElement(By.name(s)).click();
+        click(By.name(s));
     }
 
     public void deleteSelectedGroups() {
-      wd.findElement(By.name("delete")).click();
+        click(By.name("delete"));
     }
 
     public void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
     }
 }
