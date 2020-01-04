a040ad1399c1a5c1a0d05f310ec4639eca521188
Nadezhda_Barsukova <87na.vi@gmail.com>
2020-01-04 08:27:12 -0500

    commented the timeout in ApplicationManager

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/ApplicationManager.java
@@ -31,7 +31,7 @@ public class ApplicationManager {
             wd = new ChromeDriver();
         }
 
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/");
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
