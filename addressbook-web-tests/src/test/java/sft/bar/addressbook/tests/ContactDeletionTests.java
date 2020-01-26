955d3ed6240f98603ffb080efb4f695f1955d360
Nadezhda_Barsukova <87na.vi@gmail.com>
2020-01-26 20:35:56 +0300

    modified Contact and Group Helper and modified group deletion and modification and contact deletion tests

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/ContactDeletionTests.java
@@ -12,7 +12,7 @@ public class ContactDeletionTests extends TestBase {
             app.getContactHelper().createContact(new ContactData("n5", "n5", "address5", "phone5", "email5", "NadyaTest"));
         }
         int before = app.getContactHelper().getContactCount();
-        app.getContactHelper().selectContact();
+        app.getContactHelper().selectContact(before - 1);
         app.getContactHelper().deleteContact();
         int after = app.getContactHelper().getContactCount();
         Assert.assertEquals(after, before - 1);
--- addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
             app.getGroupHelper().createGroup(new GroupData("null", "NadyaTest1", "null"));
         }
         int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("NadyaTest", "NadyaTest1", "NadyaTest2"));
         app.getGroupHelper().submitGroupModification();
