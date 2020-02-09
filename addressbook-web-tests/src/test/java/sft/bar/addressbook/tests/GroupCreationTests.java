a96dd822a6dc9cd8447456c4b55aeff3a1e0aa3a
Nadezhda_Barsukova <87na.vi@gmail.com>
2020-02-10 01:08:08 +0300

    added DataProvider to GroupCreationTests

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,28 @@ import org.testng.annotations.*;
 import sft.bar.addressbook.model.GroupData;
 import sft.bar.addressbook.model.Groups;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("Nadya1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("Nadya2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("Nadya3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("NadyaTest");
         app.group().createGroup(group);
         app.goTo().groupPage();
         Groups after = app.group().all();
