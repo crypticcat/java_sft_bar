64a2aafe195aab717ed64bf3aa549b42d7a46edf
Nadezhda_Barsukova <87na.vi@gmail.com>
2020-02-12 03:07:07 +0300

    added saving to json in data generators with output file examples

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/generators/GroupDataGenerator.java
@@ -2,6 +2,8 @@ package sft.bar.addressbook.generators;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import sft.bar.addressbook.model.GroupData;
 
@@ -39,11 +41,19 @@ public class GroupDataGenerator {
             saveAsCsv(groups, new File(file));
         } else if (format.equals("xml")) {
             saveAsXml(groups, new File(file));
-            //} else if (format.equals("json")) {
-            //    saveAsJson(groups, new File(file));
+            } else if (format.equals("json")) {
+                saveAsJson(groups, new File(file));
         } else {
             System.out.println("Unrecognized format " + format);
         }
+    }
+
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
     }
 
     private void saveAsXml(List<GroupData> groups, File file) throws IOException {
