ef10e2fc48b83d62e8b725e9e60580bd07da7270
Nadezhda_Barsukova <87na.vi@gmail.com>
2020-02-18 02:42:15 +0300

    added logging to files

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/tests/TestBase.java
@@ -1,11 +1,20 @@
 package sft.bar.addressbook.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import sft.bar.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -14,9 +23,19 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    //alwaysRun - to be sure that browser is stopped
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info(m.getName() + " starting" + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info(m.getName() + " stopped");
+    }
 }
