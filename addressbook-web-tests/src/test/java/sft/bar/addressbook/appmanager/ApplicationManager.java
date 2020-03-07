5a489a5c54a72ab84a794d053480b657b754d70e
Nadezhda_Barsukova <87na.vi@gmail.com>
2020-03-08 02:08:40 +0300

    added capability to run the browser remotely

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/ApplicationManager.java
@@ -1,13 +1,17 @@
 package sft.bar.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -35,13 +39,21 @@ public class ApplicationManager {
 
         dbHelper = new DbHelper();
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new ChromeDriver();
+        if("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new ChromeDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
+
+
 
         //wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
