2a37c9e877c715713c8561359953baaca110ccf5
Nadezhda_Barsukova <87na.vi@gmail.com>
2020-03-09 16:29:36 +0300

    updated ApplicationManager

--- addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/sft/bar/addressbook/appmanager/ApplicationManager.java
@@ -53,7 +53,7 @@ public class ApplicationManager {
             WebDriverManager.chromedriver().setup();
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "Windows NT")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
